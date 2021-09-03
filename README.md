1c473ef0d20a67af3d351be234698d4b6a431793
Oungseik <mhemaungthuwin@gmail.com>
2021-09-03 11:34:27 +0630

    setup basic setting, keymapping and packages install

---
+++ README.md
@@ -0,0 +1,7 @@
+# init.lua Configuration for Neovim
+
+Configuration file in [lua](https://www.lua.org/) language.
+
+To easily copy and paste the file to the `~/.config/nvim` and use for beginner like me and also to use for learning purpose.
+
+
---
+++ init.lua
@@ -0,0 +1,35 @@
+--- Three types of configuraton options
+-- 1. Global options (vim.o)
+-- 2. Local to window (vim.wo)
+-- 3. Local to buffer (vim.bo)
+
+
+-- Locale to Buffer Options
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+
+
+-- Local to Window Options
+vim.wo.number = true
+
+
+-- KeyBinding Section
+-- set using vim.api.nvim_set_keymap({mode}, {new_keymap}, {mapped_to}, {options}) 
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true }
+keymap('n', '<c-s>', ':w<CR>', {})
+keymap('i', '<c-s>', '<Esc>:w<CR>', {})
+
+
+-- Package Management
+require('packer').startup(function() 
+    use 'wbthomason/packer.nvim'
+    use 'vimwiki/vimwiki'
+    --- add other use ... for other packages
+end)
+
+
+-- Colors
+-- if installed monoki color, uncomment the line below to use it
+-- vim.g.color = 'monoki'
